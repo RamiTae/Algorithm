42919ea644669163ae2c650e4dac9a1c6bab240d
RamiTae <trmi422@gmail.com>
2019-08-22 01:57:33 +0900

    190822 fire 시작

---
+++ 201908/201908/01. fire.cpp
@@ -0,0 +1,15 @@
+//https://level.goorm.io/exam/49051/%EB%B6%88%EC%9D%B4%EC%95%BC/quiz/1
+/*
+	1 <= (r, c) <= 1000
+	#: 벽, &: 인간, @: 불
+	&에서시작. @가 닿을 때 까지 최소 시간 구하기(1칸 남았을 때).
+	1초마다 상하좌우로 이동:: BFS사용
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+	char input[100];
+	cin >> input;
+	return 0;
+}
