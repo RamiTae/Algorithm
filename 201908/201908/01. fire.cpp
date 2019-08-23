b2e0ecc9d06ca5259a5cfe816b9e98a665336977
RamiTae <trmi422@gmail.com>
2019-08-23 21:24:36 +0900

    input 제대로 들어오는지 확인

--- 201908/201908/01. fire.cpp
+++ 201908/201908/01. fire.cpp
@@ -4,12 +4,32 @@
 	#: 벽, &: 인간, @: 불
 	&에서시작. @가 닿을 때 까지 최소 시간 구하기(1칸 남았을 때).
 	1초마다 상하좌우로 이동:: BFS사용
+	인간좌표 저장. map바꾸기.
 */
 
 #include <iostream>
 using namespace std;
+#define MAX 1000
+
+char map[MAX][MAX] = { '.', };
+
 int main() {
-	char input[100];
-	cin >> input;
+	int r, c;
+	int ans = -1;
+	cin >> r >> c;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cout << map[i][j];
+		}
+		cout << endl;
+	}
+	cout << ans;
+
 	return 0;
 }
