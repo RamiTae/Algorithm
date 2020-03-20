ccfa3dbcf32ae7a2662ee1d8e8b1020ebe9fe9f4
RamiTae <ramiTae.dev@gmail.com>
2020-03-20 20:32:56 +0900

    :sparkles: binary search - budgets. ver3

--- binarySearch/01.budgets/budgets.js
+++ binarySearch/01.budgets/budgets.js
@@ -1,24 +1,21 @@
 /*
 --- [120, 110, 140, 150]	 485
------ 2.5 번째 시도
-세팅: maxBudget = M, minBudget = 0
-* binary search로 예산을 찾음: x1
-  x1을 예산으로 사용할 경우. x1, x1을 예산으로 했을 경우의 차액을 저장: preBudget = x1, remainBudget = 차액
-* 다음 예산을 찾음: x2 = x1 + Math.floor((M - x1) / 2)
-  x2를 예산으로 사용할 경우. x2, x2 예산 차액
-  remainBudget과 x2 예산 차액을 비교
-  * x2차액 < 0
-    !=> maxBudget = x2; preBudget, remainBudget 그대로;
-    다음 예산을 찾음: x3 = Math.floor((maxBudget - minBudget) / 2)
-  * remainBudget >= x2차액
-    (equal 조건은 while loop의 처음 조건을 통과하기 위해서 필요함)
-    !=> minBudget = x2; preBudget = x2, remainBudget = x2차액;
-    다음 예산을 찾음: x3 = x2 + Math.floor((maxBudget - x2) / 2)
+----- 3 번째 시도
+minBudget = 0; maxBudget = M;
+* binary search로 예산을 찾음
+  ? nowBudget = minBudget + Math.floor((maxBudget - minBudget) / 2);
+  * 탈출 조건 확인
+    같은 budget이 두 번 나올 경우 그 budget이 최선인것으로 판단.
+    => 범위를 좁혀나가며 탐색하기 때문에 이전 결과의 budget과 현 budget이 같을 경우를 최선으로 판단
+  * if(예산 계산 결과 < 0)
+    ? maxBudget = nowBudget;
+    ? preBudget = nowB;
+  * if else(예산 계산 결과 === 0)
+    ! result = nowBudget;
+    ! break;
   * else
-    => 이론상 xn이 점점 커지기 때문에 이 else문을 탈 일이 없음. 뭔가가 잘못된 것이다..!
-* 탈출 조건
-  같은 budget이 두 번 나올 경우 그 budget이 최선인것으로 판단.
-  => 다회 방문 여부를 저장해야하기 때문에 binary search로 찾은 budget과 remainBudget들을 object로 저장
+    ? minBudget = nowBudget;
+    ? preBudget = nowB;
 */
 
 function solution(budgets, M) {
@@ -29,46 +26,47 @@ function solution(budgets, M) {
     }
     return acc + val;
   }, 0);
-  let calculatedBudgets = {};
 
   if (M - sumBudgets < 0) {
     // 모든 요청이 배정될 수 없음
-    let maxBudget = M;
     let minBudget = 0;
-    result = Math.floor(M / 2);
+    let maxBudget = M;
     let preBudget = 0;
-    calculatedBudgets[preBudget] = 100001;
 
-    while (calculatedBudgets[result] === undefined) {
+    while (minBudget >= 0 && maxBudget <= M) {
       //탈출조건: 같은 budget이 두 번 나올 경우
-      let remainBudget =
+      let nowBudget = minBudget + Math.floor((maxBudget - minBudget) / 2);
+      if (nowBudget === preBudget) {
+        result = nowBudget;
+        break;
+      }
+
+      let calculateBudget =
         M -
         budgets.reduce((acc, val) => {
-          if (val > result) {
-            return acc + result;
+          if (val > nowBudget) {
+            return acc + nowBudget;
           } else {
             return acc + val;
           }
         }, 0);
-      calculatedBudgets[result] = remainBudget;
-      let preRemainBudget = calculatedBudgets[preBudget];
 
-      if (remainBudget < 0) {
-        maxBudget = result;
-        result = minBudget + Math.floor((maxBudget - minBudget) / 2);
-        console.log("** remainBudget < 0\n", { maxBudget, minBudget, result });
-      } else if (preRemainBudget >= remainBudget) {
-        minBudget = result;
-        result += Math.floor((maxBudget - result) / 2);
-        console.log("** preRemainBudget >= remainBudget\n", { maxBudget, minBudget, result });
-      } else {
-        console.log("something wrong!!");
+      if (calculateBudget === 0) {
+        result = nowBudget;
+        // console.log({ calculateBudget, minBudget, maxBudget, nowBudget });
         break;
+      } else if (calculateBudget < 0) {
+        maxBudget = nowBudget;
+        preBudget = nowBudget;
+        // console.log({ calculateBudget, minBudget, maxBudget, nowBudget });
+      } else {
+        minBudget = nowBudget;
+        preBudget = nowBudget;
+        // console.log({ calculateBudget, minBudget, maxBudget, nowBudget });
       }
     }
   }
 
-  console.log(calculatedBudgets);
   return result;
 }
 
@@ -82,11 +80,12 @@ expect = 1;
 
 budgets = [1000, 1000, 1000, 1, 100];
 M = 500;
+expect = 133;
 
 let result = solution(budgets, M);
-console.log({ result });
+console.log(`result: ${typeof result} ${result}`);
 if (result === expect) {
-  console.log("pass");
+  console.log("\u001b[36m pass \u001b[0m");
 } else {
-  console.log("non pass");
+  console.log("\u001b[31m non pass \u001b[0m");
 }
